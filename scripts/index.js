62c78cbd254a006fb4b08ca46281516fddbced0b
Kozazavr <sohee@yandex.ru>
2020-09-28 23:15:42 +0300

    commit before merge

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,10 @@
-let page = document.querySelector('.page');
-let page__container = page.querySelector('.page__container');
-let popup = page__container.querySelector('.popup');
-let popup__container = popup.querySelector('.popup__container');
-let popup__name = popup__container.querySelector('.popup__name');
-let popup__occupation = popup__container.querySelector('.popup__occupation');
-let profile = page__container.querySelector('.profile');
-let profile__info = profile.querySelector('.profile__info');
-let profile__button = profile__info.querySelector('.profile__button');
-let profile__name = profile__info.querySelector('.profile__name');
-let profile__occupation = profile__info.querySelector('.profile__occupation');
-let popup__plus = popup__container.querySelector('.popup__plus');
-let popup__button = popup__container.querySelector('.popup__button');
-
+let popup = document.querySelector('.popup');
+let popup__name = document.querySelector('.popup__name');
+let popup__occupation = document.querySelector('.popup__occupation');
+let profile__button = document.querySelector('.profile__button');
+let profile__name = document.querySelector('.profile__name');
+let profile__occupation = document.querySelector('.profile__occupation');
+let popup__plus = document.querySelector('.popup__plus');
 
 function openPopup () {
   popup.classList.toggle('popup_opened');
