2e0d50613d1829cbf2a4ff1667a6ff324ce92c4a
Kozazavr <sohee@yandex.ru>
2020-09-30 17:08:00 +0300

    popup plus fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,36 @@
 let popup = document.querySelector('.popup');
-let popup__name = document.querySelector('.popup__name');
-let popup__occupation = document.querySelector('.popup__occupation');
-let profile__button = document.querySelector('.profile__button');
-let profile__name = document.querySelector('.profile__name');
-let profile__occupation = document.querySelector('.profile__occupation');
-let popup__plus = document.querySelector('.popup__plus');
+let popupName = document.querySelector('.popup__name');
+let popupOccupation = document.querySelector('.popup__occupation');
+let profileButton = document.querySelector('.profile__button');
+let profileName = document.querySelector('.profile__name');
+let profileOccupation = document.querySelector('.profile__occupation');
+let popupPlus = document.querySelector('.popup__plus');
 
-function openPopup () {
-  popup.classList.toggle('popup_opened');
-  popup__name.value = profile__name.textContent;
-  popup__occupation.value = profile__occupation.textContent;
+function eventPopup () {
+  if (popup.classList.contains('popup_opened') === false) {
+        popup.classList.add('popup_opened');
+        popupName.value = profileName.textContent;
+        popupOccupation.value = profileOccupation.textContent;
+
+  } else {
+    popup.classList.remove('popup_opened');
+  }  
 }
 
-profile__button.addEventListener('click', openPopup);
-
-
-function closePopup () {
-  popup.classList.toggle('popup_opened');
-}
-
-popup__plus.addEventListener('click', closePopup);
-
-
- function formSubmitHandler (evt) {
-    evt.preventDefault();
-    let valueName = popup__name.value;
-    profile__name.textContent = valueName;
-    let valueOccupation = popup__occupation.value;
-    profile__occupation.textContent = valueOccupation;
-    closePopup();
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  let valueName = popupName.value;
+  profileName.textContent = valueName;
+  let valueOccupation = popupOccupation.value;
+  profileOccupation.textContent = valueOccupation;
+  eventPopup();
   
  }
 
+profileButton.addEventListener('click', eventPopup);
+popupPlus.addEventListener('click', eventPopup);
 popup.addEventListener('submit', formSubmitHandler);
+
 
 
 
