c968ba897de5054eb6fe50373ddc8e62347e556c
Kozazavr <sohee@yandex.ru>
2020-11-10 23:03:58 +0300

    realize Popup-class

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,7 @@ import {initialCards} from './array.js';
 import Card from './card.js';
 import FormValidator from './FormValidator.js';
 import {Section} from './Section.js';
+import {Popup} from './Popup.js';
 
 const selectors = {
   formSelector: '.popup__container',  
@@ -20,34 +21,15 @@ const inputName = document.querySelector('.popup__input_type_name');
 const inputJob = document.querySelector('.popup__input_type_job');
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
-const popupCloseProfile = document.querySelector('.popup__close_profile');
-const popupCloseAddImages = document.querySelector('.popup__close_add-images');
 const submitAddImages = document.querySelector('.popup__button_create');
 const submitProfile  = document.querySelector('.popup__button_save');
 const popupPicture = document.querySelector('.popup__picture');
 const popupPictureTitle = document.querySelector('.popup__picture-title');
-const popupCloseViewImage= document.querySelector('.popup__close_view-image');
 const nameImage = document.querySelector('.popup__input_type_image-name');
 const linkImage = document.querySelector('.popup__input_type_image-link');
 const popupViewImages = document.querySelector('.popup_view-images');
+
           
-function closeEsc (evt) {
-  if(evt.key === 'Escape') {
-    const currentPopup = document.querySelector('.popup_opened');
-    closePopup(currentPopup);
-  }
-}
-
-function openPopup (popup) {
-  popup.classList.add('popup_opened');
-  document.addEventListener('keydown', closeEsc); 
-}
-
-function closePopup (popup) {
-  popup.classList.remove('popup_opened');
-  document.removeEventListener('keydown', closeEsc);
-}
-
 function clearInputs (popup) {
   const form = popup.querySelector('.popup__container');
   form.reset();
@@ -58,8 +40,12 @@ function clickCard(card) {
     const targetTitle = card.querySelector('.card__title');
     popupPicture.src = targetImage.src;
     popupPictureTitle.textContent = targetTitle.textContent;
-    openPopup(popupViewImages);
+    viewPopup.open(popupViewImages);
   }
+
+const profilePopup = new Popup('.popup_profile');
+const addPopup = new Popup('.popup_add-images');
+const viewPopup = new Popup('.popup_view-images');
 
 function renderer(item) {
   const card = new Card(item, '#card').generateCard();
@@ -81,7 +67,7 @@ function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
-  closePopup(popupProfile);
+  profilePopup.close(popupProfile);
 }
 
 function formSubmitImages (evt) {
@@ -90,7 +76,7 @@ function formSubmitImages (evt) {
     link: linkImage.value
   }];
   rendererSection(item);
-  closePopup(popupAddImages);  
+  addPopup.close(popupAddImages);
 }
 
 profileButtonAddImages.addEventListener('click', function () {
@@ -98,7 +84,7 @@ profileButtonAddImages.addEventListener('click', function () {
   validatePopupAddImages.hideInputError(popupAddImages, nameImage);
   validatePopupAddImages.hideInputError(popupAddImages, linkImage);
   clearInputs(popupAddImages);
-  openPopup(popupAddImages);
+  addPopup.open(popupAddImages);
 });  
 
 profileButton.addEventListener('click', function () {  
@@ -107,31 +93,21 @@ profileButton.addEventListener('click', function () {
   validatePopupProfile.activeButton(submitProfile); 
   validatePopupProfile.hideInputError(popupProfile, inputName);
   validatePopupProfile.hideInputError(popupProfile, inputJob);
-  openPopup(popupProfile);
+  profilePopup.open(popupProfile);
 }); 
 
 function closePopupOverlay(evt) {
   const openedPopup = document.querySelector('.popup_opened');
   if(evt.target.classList.contains('popup_opened')) {
-    closePopup(openedPopup);
+    profilePopup.close(openedPopup);
+    addPopup.close(openedPopup);
+    viewPopup.close(openedPopup);
   }
 }
 
 popupProfile.addEventListener('click', closePopupOverlay);
 popupAddImages.addEventListener('click', closePopupOverlay);
 popupViewImages.addEventListener('click', closePopupOverlay);
-
-popupCloseProfile.addEventListener('click', function () { 
-  closePopup(popupProfile);
-});  
-
-popupCloseAddImages.addEventListener('click', function () {  
-  closePopup(popupAddImages);
-});  
-
-popupCloseViewImage.addEventListener('click', function () {  
-  closePopup(popupViewImages);
-});  
 
 popupProfile.addEventListener('submit', formSubmitHandler);
 popupAddImages.addEventListener('submit', formSubmitImages);
