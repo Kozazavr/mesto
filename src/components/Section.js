231a24d8f75da6d48e21a19dd27df30d8458cb7d
Kozazavr <sohee@yandex.ru>
2020-11-22 20:56:43 +0300

    m

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,15 +2,9 @@ export default class Section {
   constructor({items, renderer}, selectorContainer) {
     this._items = items;
     this.renderer = renderer;
-    this._selectorContainer = selectorContainer;
+    this._container = document.querySelector(selectorContainer);
   }
     
-  _getContainer() {
-    const cardContainer = document
-    .querySelector(this._selectorContainer);
-    return cardContainer;
-  }
-
   renderCards() {
     this._items.map(item => {
       this.renderer(item);
