bfb9cd400368621d37b47141581e1e8ed40ec003
Kozazavr <sohee@yandex.ru>
2020-11-25 23:38:22 +0300

    fix Popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,22 +1,24 @@
 export default class Popup {
   constructor(selectorPopup) {
     this._openPopup = document.querySelector(selectorPopup);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
     this._openPopup.classList.add('popup_opened');
-    document.addEventListener('keydown', this._handleEscClose.bind(this));  
+    document.addEventListener('keydown', this._handleEscClose);  
   }
 
   _handleEscClose(evt) {
     if(evt.key === 'Escape') {
       this.close();
+      console.log('AAA');
     }
   }
   
   close() {
     this._openPopup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', this._handleEscClose.bind(this));
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
   setEventListener() {
